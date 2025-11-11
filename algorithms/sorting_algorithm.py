ec2de441572b2dbe63d8784c240207dee2a81120
Nikita Karasevych <21usual8@gmail.com>
2025-11-11 13:30:18 +0200

    Sorting algorithms update 1.2, changing convertor module file

--- algorithms/sorting_algorithm.py
+++ algorithms/sorting_algorithm.py
@@ -1,11 +1,11 @@
 import asyncio
 import json
 import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 from algorithms.category import catalog, keywords
+from bot.convertor import save_to_json2
 
 categorized = {cat: [] for cat in keywords.keys()}
+default_category = {cat: [] for cat in keywords.keys()} #TODO
 
 
 async def sorting(data: dict, catalog):
@@ -16,38 +16,12 @@ async def sorting(data: dict, catalog):
         found = False
 
         for k, v in keywords.items():
-            if any(word in msg_lower for word in v): #если слово из ключевых слов есть в сообщении
-                    categorized[k].append(msg) #почкему добавляем по значению ключа, а не по ключу?
+            if any(word in msg_lower for word in v or k): 
+                    categorized[k].append(msg) 
                     found = True
                     
             if not found:
-                return
-    return categorized
+                return default_category[k].append(msg) #TODO
+    return await save_to_json2(categorized)
     
 
-async def save_to_json2(categorized: dict, filename: str = "sorting_messages.json"):
-    """Beta version of message sorter хранителя"""
-
-    # Если файл не существует — создаём базовую структуру
-    if not os.path.exists(filename):
-        with open(filename, "w", encoding="utf-8") as f:
-            json.dump({}, f, ensure_ascii=False, indent=4)
-
-    # Загружаем текущие данные
-    with open(filename, "r", encoding="utf-8") as f:
-        try:
-            data = json.load(f)
-        except json.JSONDecodeError:
-            data = {}
-
-    # Объединяем старые и новые категории
-    for category, messages in categorized.items():
-        if category not in data:
-            data[category] = []  
-        for msg in messages:
-            if msg not in data[category]:  
-                data[category].append(msg)
-
-    # Сохраняем обратно в JSON
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
--- bot/convertor.py
+++ bot/convertor.py
@@ -1,6 +1,10 @@
 import os
 import json
-def save_to_json(new_text: str, filename: str = "messages.json"):
+import asyncio
+from algorithms.sorting_algorithm import sorting
+from algorithms.category import catalog
+from algorithms.export import export_to_txt
+async def save_to_json(new_text: str, filename: str = "messages.json"):
     if not os.path.exists(filename):
         with open(filename, "w", encoding="utf-8") as f:
             json.dump({"messages": []}, f, ensure_ascii=False, indent=4)
@@ -12,3 +16,34 @@ def save_to_json(new_text: str, filename: str = "messages.json"):
 
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+    return sorting(data, catalog)
+
+
+async def save_to_json2(categorized: dict, filename: str = "sorting_messages.json"):
+    """Beta version of message sorter хранителя"""
+
+    # Если файл не существует — создаём базовую структуру
+    if not os.path.exists(filename):
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump({}, f, ensure_ascii=False, indent=4)
+
+    # Загружаем текущие данные
+    with open(filename, "r", encoding="utf-8") as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            data = {}
+
+    # Объединяем старые и новые категории
+    for category, messages in categorized.items():
+        if category not in data:
+            data[category] = []  
+        for msg in messages:
+            if msg not in data[category]:  
+                data[category].append(msg)
+
+    # Сохраняем обратно в JSON
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    return export_to_txt(data) #TODO
