d742138f9b74f108eda0faa84ca8bd318725d00a
Gitmon-1 <nazarbohdan2010@gmail.com>
2025-11-18 14:59:09 +0200

    added kb add menu

--- kb/kb.py
+++ kb/kb.py
@@ -3,10 +3,52 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 # Главное меню
 main_menu_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text='Add')], 
-        [KeyboardButton(text='Show')],
-        [KeyboardButton(text='Delete')]
+        [KeyboardButton(text='Добавить')], 
+        [KeyboardButton(text='Поиск')],
+        [KeyboardButton(text='Удалить')]
     ],
     resize_keyboard=True,
     input_field_placeholder='Выберите пункт меню...'
 )   
+
+# Добавить
+#   текст (заметка)
+#   категория
+#       подкатегория
+#           текст (заметка)
+
+# текст, категория
+add_menu_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Текст')], 
+        [KeyboardButton(text='Категория')]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню...'
+)
+
+# подкатегория
+under_category_menu_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Подкатегория')]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню...'
+)
+# текст
+text_menu_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Текст')]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню...'
+)
+
+# Поиск
+#   по категорри
+#   по подкатегории
+
+# Удалить
+#   категорию
+#   подкатегорию
+#   текст (заметка)
